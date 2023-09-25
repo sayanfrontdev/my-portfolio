a7b0567cbf5565e90f1285b1f7493e77e30a57bc
Sayan <sayanmanda1922001@gmail.com>
2023-09-25 17:41:04 +0530

    change contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("http://localhost:3000/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
